aaf136341efe40e5e4e9da6ddbe18a05d2c46fb3
sammy wlodawski <samwlodawski@gmail.com>
2024-08-26 11:40:28 -0400

    Added the post and get all routes to db.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,34 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Data Posted');
+
+  const contactDb = await openDB('contact', 1);
+
+  const tx = contactDb.transaction('contact', 'readwrite');
+
+  const store = tx.objectStore('contact');
+
+  const request = store.add({data});
+
+  const result = await request;
+  console.log('Data stored successfully', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const contactDb = await openDB('contact', 1);
+
+  const tx = contactDb.transaction('contact', 'readonly');
+
+  const store = tx.objectStore('contact');
+
+  const request = store.getAll();
+
+  const result = await(request);
+  console.log('result.value', value);
+  return result;
+};
 
 initdb();
